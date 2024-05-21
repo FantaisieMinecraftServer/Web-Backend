7d74d0e06c7aeed6045450a7cc5839f67eace17e
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2024-05-21 12:35:59 +0900

    Feat: リーダーボードを追加

--- app/lib/database.go
+++ app/lib/database.go
@@ -17,5 +17,21 @@ func GetDBConnection() *sql.DB {
 	if err != nil {
 		panic(err.Error())
 	}
+
+	db.SetConnMaxLifetime(20)
+	db.SetConnMaxIdleTime(20)
+
+	return db
+}
+
+func GetLBDBConnection() *sql.DB {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db, err := sql.Open("mysql", os.Getenv("LBDB_URL"))
+	if err != nil {
+		panic(err.Error())
+	}
 	return db
 }
